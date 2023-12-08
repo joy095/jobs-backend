a30ef955000d72911664dc588386bfc975e1c34a
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-12-08 12:03:39 +0530

    test api

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name! is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+});
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ PORT = process.env.PORT || 3500;
 
 connectDB();
 
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
